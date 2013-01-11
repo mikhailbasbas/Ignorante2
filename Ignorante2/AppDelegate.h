9a7c2d5fb9e4a32c2814801c7ab3897bc1d4b2d9
MikhailBasbas <mikhail.basbas@gmail.com>
2013-01-11 09:14:11 +0800

    initial commit

---
+++ Ignorante2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ignorante2
+//
+//  Created by Mikhail Lomibao Basbas on 1/11/13.
+//  Copyright (c) 2013 Mikhail Lomibao Basbas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ignorante2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ignorante2
+//
+//  Created by Mikhail Lomibao Basbas on 1/11/13.
+//  Copyright (c) 2013 Mikhail Lomibao Basbas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
